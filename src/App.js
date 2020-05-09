fc2040b603e22f5afe4f28ce45cd6971059d8b78
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2020-05-09 12:03:19 +0900

    #2 React Styled Components Preview

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import styled from "styled-components";
 
-class App {
-    render() {
-        return <div>Hello</div>;
-    }
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Button danger>Hello</Button>
+        <Button>Hello</Button>
+      </Container>
+    );
+  }
 }
 
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #bdc3c7;
+`;
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: black;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${(props) => (props.danger ? "#e74c3c" : "#2ecc71")};
+`;
 export default App;
